40b64f4188e217ef4d4e5d1d003b57b6d17f8bc1
Maurice CODJO <codjo@africatech.ci>
2026-07-30 15:27:15 +0100

    Implement structural updates and optimizations across multiple modules

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Landing from './pages/Landing'
 import Hub from './pages/Hub'
 import OptimizeImages from './pages/OptimizeImages'
 import OptimizeVideos from './pages/OptimizeVideos'
+import DownloadVideo from './pages/DownloadVideo'
 import SignImages from './pages/SignImages'
 import SmoothImages from './pages/SmoothImages'
 import Security from './pages/Security'
@@ -19,6 +20,7 @@ function App() {
         <Route path="/app" element={<Hub />} />
         <Route path="/app/images" element={<OptimizeImages />} />
         <Route path="/app/videos" element={<OptimizeVideos />} />
+        <Route path="/app/download" element={<DownloadVideo />} />
         <Route path="/app/sign" element={<SignImages />} />
         <Route path="/app/smooth" element={<SmoothImages />} />
         <Route path="/security" element={<Security />} />
